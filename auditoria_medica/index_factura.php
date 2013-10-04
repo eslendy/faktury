e108d02a85fc2ede7c1e794b7c7e4498852b378b
carlos Casadiego <carlos@xng.bz>
2013-10-04 00:39:00 -0300

    push files

--- auditoria_medica/index_factura.php
+++ auditoria_medica/index_factura.php
@@ -86,7 +86,7 @@ if (count($AlarmaGlosa1) > 0) {
         foreach ($AlarmaGlosa1 as $key => $ad) {
             ?>
             <div class="links-sin-devoluciones">
-                <a class="agregarNuevaGlosa link-<?php echo $ad['idFactura'] ?>"  role="button" data-auditoria='<?php echo $ad['idauditoria_medica']; ?>' data-toggle="modal" href="#agregarNuevaGlosa" data-record="<? echo $ad['idFactura']; ?>" <? echo (($_REQUEST['section'])) ? 'data-section="' . $_REQUEST['section'] . '"' : ''; ?> <? echo (($_REQUEST['action'])) ? 'data-action="' . $_REQUEST['action'] . '"' : ''; ?>><?php echo ($key + 1); ?>. Auditoria Medica con glosa inicial realizada el <?php echo $ad['glosa_fecha_glosa_1'] ?> para la Factura N° <?php echo $ad['numero_factura'] ?></a>
+                <a class="agregarNuevaGlosa link-<?php echo $ad['idFactura'] ?>"  role="button" data-auditoria='<?php echo $ad['idauditoria_medica']; ?>' data-toggle="modal" href="#agregarNuevaGlosa" data-record="<? echo $ad['idFactura']; ?>" <? echo (($_REQUEST['section'])) ? 'data-section="' . $_REQUEST['section'] . '"' : ''; ?> <? echo (($_REQUEST['action'])) ? 'data-action="' . $_REQUEST['action'] . '"' : ''; ?>><?php echo ($key + 1); ?>. Auditoria Medica con primera respuesta de glosa realizada el <?php echo $ad['glosa_fecha_glosa_1'] ?> para la Factura N° <?php echo $ad['numero_factura'] ?></a>
             </div>
 
             <?php
